d7c75ed43e94ac97fee9723098679c65e7d5ac40
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2024-01-17 05:40:29 +0100

    Check if user exists

--- Classes/Command/NotificationCommand.php
+++ Classes/Command/NotificationCommand.php
@@ -64,76 +64,76 @@ class NotificationCommand extends Command {
 		}
 
 		foreach ($notifications as $notification) {
-			$user = $notification->getUser();
-			// verify if user is still active?
-			$me = $user->getTxFkupeopleFkudbid();
-			if ($me > 0) {
-				$days = $notification->getDays();
-				foreach ($masters as $master) {
-					$interval = intval($today->diff($master->getDate())->days);
-					if ($interval == $days) {
-						$myMaster = Utilities::identifyUser($master,$me);
-						if ($notification->getRule()->getNid() == 28 and $master->getDate()->format('H.i') != $settings['defaultStartTime']) {
-							// notification if service takes place to unusual time
-							if ($notification->getRule()->getMessage()) {
-								$text = $user->getTxFkupeopleNotificationCacheday();
-								if ($interval > 1) {
-									$fill1 = $days.' Tagen';
-								} else {
-									$fill1 = '1 Tag';
-								}
-								$fill2 = utf8_encode(strftime('%e. %B', $master->getDate()->getTimestamp()));
-								$fill3 = $master->getDate()->format('H.i');
-								$text .= sprintf($notification->getRule()->getMessage(), $fill1, $fill2, $fill3);
-								$text .= chr(13).'-----'.chr(13);
-								$user->setTxFkupeopleNotificationCacheday($text);
-								$userRepository->update($user);
-						        $io->writeln('Notification created for user '.$user->getName().' based on notification rule 28.');
-							}
-						} elseif ($notification->getRule()->getNid() == 1 and $myMaster !== false) {
-							// notification is created even if user is not involved but any reason for notifications is given
-							$text = $user->getTxFkupeopleNotificationCacheday();
-							// no check for $notification->getTiming because it is supposed to be 1 in every case
-							if ($interval > 1) {
-								$fill1 = $days.' Tagen';
-							} else {
-								$fill1 = '1 Tag';
-							}
-							$fill2 = utf8_encode(strftime('%e. %B', $master->getDate()->getTimestamp()));
-							$fill3 = $master->getServiceTopic();
-							$fill4 = $master->getTeensTopic1();
-							$fill5 = $master->getTeensTopic2();
-							$fill6 = $master->getKidsTopic();
-							foreach  ($myMaster['notifications'] as $ruleId) {
-								if ($rule = $notificationruleRepository->findSingle('fku_planning',$ruleId)->getFirst()) {
-									$text .= sprintf($rule->getMessage(), $fill1, $fill2, $fill3, $fill4, $fill5, $fill6);
-									$text .= chr(13).'-----'.chr(13);
-								}
-							}
-							$user->setTxFkupeopleNotificationCacheday($text);
-							$userRepository->update($user);
-					        $io->writeln('Notification(s) created for user '.$user->getName().' based on notification rule 1.');
-						} else {
-							if ($myMaster !== false and in_array($notification->getRule()->getNid(),$myMaster['notifications'])) {
-								// notification is only created if user is involved in made change
+			if ($user = $notification->getUser()) {
+				$me = $user->getTxFkupeopleFkudbid();
+				if ($me > 0) {
+					$days = $notification->getDays();
+					foreach ($masters as $master) {
+						$interval = intval($today->diff($master->getDate())->days);
+						if ($interval == $days) {
+							$myMaster = Utilities::identifyUser($master,$me);
+							if ($notification->getRule()->getNid() == 28 and $master->getDate()->format('H.i') != $settings['defaultStartTime']) {
+								// notification if service takes place to unusual time
 								if ($notification->getRule()->getMessage()) {
 									$text = $user->getTxFkupeopleNotificationCacheday();
-									// no check for $notification->getTiming because it is supposed to be 1 in every case
 									if ($interval > 1) {
 										$fill1 = $days.' Tagen';
 									} else {
 										$fill1 = '1 Tag';
 									}
 									$fill2 = utf8_encode(strftime('%e. %B', $master->getDate()->getTimestamp()));
-									$fill3 = $master->getServiceTopic();
-									$fill4 = $master->getTeensTopic1();
-									$fill5 = $master->getTeensTopic2();
-									$fill6 = $master->getKidsTopic();
-									$text .= sprintf($notification->getRule()->getMessage(), $fill1, $fill2, $fill3, $fill4, $fill5, $fill6);
+									$fill3 = $master->getDate()->format('H.i');
+									$text .= sprintf($notification->getRule()->getMessage(), $fill1, $fill2, $fill3);
 									$text .= chr(13).'-----'.chr(13);
 									$user->setTxFkupeopleNotificationCacheday($text);
 									$userRepository->update($user);
-							        $io->writeln('Notification created for user '.$user->getName().' based on notification rule '.$notification->getRule()->getNid().'.');
+									$io->writeln('Notification created for user '.$user->getName().' based on notification rule 28.');
+								}
+							} elseif ($notification->getRule()->getNid() == 1 and $myMaster !== false) {
+								// notification is created even if user is not involved but any reason for notifications is given
+								$text = $user->getTxFkupeopleNotificationCacheday();
+								// no check for $notification->getTiming because it is supposed to be 1 in every case
+								if ($interval > 1) {
+									$fill1 = $days.' Tagen';
+								} else {
+									$fill1 = '1 Tag';
+								}
+								$fill2 = utf8_encode(strftime('%e. %B', $master->getDate()->getTimestamp()));
+								$fill3 = $master->getServiceTopic();
+								$fill4 = $master->getTeensTopic1();
+								$fill5 = $master->getTeensTopic2();
+								$fill6 = $master->getKidsTopic();
+								foreach  ($myMaster['notifications'] as $ruleId) {
+									if ($rule = $notificationruleRepository->findSingle('fku_planning',$ruleId)->getFirst()) {
+										$text .= sprintf($rule->getMessage(), $fill1, $fill2, $fill3, $fill4, $fill5, $fill6);
+										$text .= chr(13).'-----'.chr(13);
+									}
+								}
+								$user->setTxFkupeopleNotificationCacheday($text);
+								$userRepository->update($user);
+								$io->writeln('Notification(s) created for user '.$user->getName().' based on notification rule 1.');
+							} else {
+								if ($myMaster !== false and in_array($notification->getRule()->getNid(),$myMaster['notifications'])) {
+									// notification is only created if user is involved in made change
+									if ($notification->getRule()->getMessage()) {
+										$text = $user->getTxFkupeopleNotificationCacheday();
+										// no check for $notification->getTiming because it is supposed to be 1 in every case
+										if ($interval > 1) {
+											$fill1 = $days.' Tagen';
+										} else {
+											$fill1 = '1 Tag';
+										}
+										$fill2 = utf8_encode(strftime('%e. %B', $master->getDate()->getTimestamp()));
+										$fill3 = $master->getServiceTopic();
+										$fill4 = $master->getTeensTopic1();
+										$fill5 = $master->getTeensTopic2();
+										$fill6 = $master->getKidsTopic();
+										$text .= sprintf($notification->getRule()->getMessage(), $fill1, $fill2, $fill3, $fill4, $fill5, $fill6);
+										$text .= chr(13).'-----'.chr(13);
+										$user->setTxFkupeopleNotificationCacheday($text);
+										$userRepository->update($user);
+										$io->writeln('Notification created for user '.$user->getName().' based on notification rule '.$notification->getRule()->getNid().'.');
+									}
 								}
 							}
 						}
